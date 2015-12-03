2887387d2490aef10d8d7891e11f38af19bf26ef
shreyabhatnagar <simshreya@gmail.com>
2015-12-03 11:56:29 +0530

    Updated code to fix the issue with the error in connection.

---
+++ src/main/com/cisco/dse/global/migration/config/BaseAction.java
@@ -0,0 +1,35 @@
+package com.cisco.dse.global.migration.config;
+
+import org.apache.log4j.Logger;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import com.cisco.dse.global.migration.productlanding.ProductLandingVariation6;
+
+public abstract class BaseAction {
+
+	static Logger log = Logger.getLogger(ProductLandingVariation6.class);
+
+	int count = 0;
+
+	protected Document getConnection(String loc) {
+		Document doc = null;
+		try {
+			log.debug("in the getConnection method.");
+			Connection connection = Jsoup.connect(loc);
+			if (connection != null) {
+				doc = connection.get();
+			} else if (count < 10) {
+				count++;
+				log.debug("Trying to reconnect count : " + count);
+				getConnection(loc);
+			}
+		} catch (Exception e) {
+			log.error("Exception : ", e);
+		}
+
+		return doc;
+	}
+
+}
