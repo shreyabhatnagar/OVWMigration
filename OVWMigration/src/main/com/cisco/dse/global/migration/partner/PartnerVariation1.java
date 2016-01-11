cdc5b1f853a056678ff03cc55b7680f81ec30a20
azizuddin khan <azizukha@cisco.com>
2016-01-11 16:59:24 +0530

    Updated partner/PartnerVariation1.java

--- OVWMigration/src/main/com/cisco/dse/global/migration/partner/PartnerVariation1.java
+++ OVWMigration/src/main/com/cisco/dse/global/migration/partner/PartnerVariation1.java
@@ -47,6 +47,9 @@ public class PartnerVariation1 extends BaseAction {
 			String partnerRight = pagePropertiesPath+"content_parsys/partner_central/layout-partner-central/gd12v2/gd12v2-right";
 			String partnerMid = pagePropertiesPath+"content_parsys/partner_central/layout-partner-central/gd13v2";
 			String partnerBottom = pagePropertiesPath+"content_parsys/partner_central/layout-partner-central/gd11v1/gd11v1-mid";
+			String partnerDownLeft = pagePropertiesPath+"content_parsys/partner_central/layout-partner-central/gd11v1_0/gd11v1-mid";
+			String partnerDownMid = pagePropertiesPath+"content_parsys/partner_central/layout-partner-central/gd13v2_0";
+			String partnerDownBottom = pagePropertiesPath+"content_parsys/partner_central/layout-partner-central/gd11v1_1/gd11v1-mid";
 
 			log.debug("Path is "+partnerLeft);
 			log.debug("Path is "+partnerRight);
@@ -61,12 +64,18 @@ public class PartnerVariation1 extends BaseAction {
 			javax.jcr.Node partnerRightNode = null;
 			javax.jcr.Node partnerMidNode = null;
 			javax.jcr.Node partnerBottomNode = null;
+			javax.jcr.Node partnerDownLeftNode = null;
+			javax.jcr.Node partnerDownMidNode = null;
+			javax.jcr.Node partnerDownBottomNode = null;
 			javax.jcr.Node pageJcrNode = null;
 
 			partnerLeftNode = session.getNode(partnerLeft);
 			partnerRightNode = session.getNode(partnerRight);
 			partnerMidNode = session.getNode(partnerMid);
 			partnerBottomNode = session.getNode(partnerBottom);
+			partnerDownLeftNode = session.getNode(partnerDownLeft);
+			partnerDownMidNode = session.getNode(partnerDownMid);
+			partnerDownBottomNode = session.getNode(partnerDownBottom);
 			pageJcrNode = session.getNode(pagePropertiesPath);
 
 			try {
@@ -86,8 +95,24 @@ public class PartnerVariation1 extends BaseAction {
 				try{
 					log.debug("Start Hero Element Migration");
 					Element heroEle = doc.select("div.c50v3-pilot").first();
-					migrateHero(heroEle , partnerLeftNode , locale , urlMap);
-					log.debug("Hero Element Migrated");
+					if(heroEle != null){
+						Node heroNode = partnerLeftNode.hasNode("hero_large")?partnerLeftNode.getNode("hero_large"):null;
+						Node heroDownNode = partnerDownLeftNode.hasNode("hero_full")?partnerDownLeftNode.getNode("hero_full"):null;
+						if(heroNode != null){
+							migrateHero(heroNode, heroEle, locale, urlMap);
+							log.debug("Hero Element Migrated");
+						}else{
+							sb.append(Constants.HERO_CONTENT_NODE_NOT_FOUND);
+						}
+						if(heroDownNode != null){
+							migrateHero(heroDownNode, heroEle, locale, urlMap);
+							log.debug("Hero Element Migrated");
+						}else{
+							sb.append(Constants.HERO_CONTENT_NODE_NOT_FOUND);
+						}
+					}else{
+						sb.append(Constants.HERO_CONTENT_PANEL_ELEMENT_NOT_FOUND);
+					}
 				}catch(Exception e){
 					sb.append(Constants.EXCEPTION_IN_HERO_MIGRATION);
 					log.debug("Exception in Hero Element Migration"+e);
@@ -110,8 +135,16 @@ public class PartnerVariation1 extends BaseAction {
 				try{
 					log.debug("start Html Blobs migration");
 					Element midEle = doc.select("div.gd13v2-pilot").first();
-					migrateHtmlBlobs(midEle , partnerMidNode, locale , urlMap);
-					log.debug("Html Blobs Element Migrated");
+					boolean loggedIn = true;
+					if(midEle != null){
+						migrateHtmlBlobs(midEle , partnerMidNode, locale , urlMap , loggedIn);
+						log.debug("Html Blobs Element Migrated");
+						loggedIn = false;
+						migrateHtmlBlobs(midEle , partnerDownMidNode, locale , urlMap , loggedIn);
+						log.debug("Html Blobs Element Migrated");
+					}else{
+						sb.append(Constants.HTMLBLOB_CONTENT_DOES_NOT_EXIST);
+					}
 				}catch(Exception e){
 					sb.append(Constants.EXCEPTION_IN_HTMLBLOB);
 					log.debug("Exception in Top Right Element Migration"+e);
@@ -122,8 +155,14 @@ public class PartnerVariation1 extends BaseAction {
 				try{
 					log.debug("start Text Migration");
 					Element textEle = doc.select("div.gd11-pilot").last().getElementsByClass("compact").first();
-					migrateText(textEle , partnerBottomNode , locale, urlMap);
-					log.debug("Text is Migrated");
+					if(textEle != null){
+						migrateText(textEle , partnerBottomNode , locale, urlMap);
+						log.debug("Text is Migrated");
+						migrateText(textEle , partnerDownBottomNode , locale, urlMap);
+						log.debug("Text is Migrated");
+					}else{
+						sb.append(Constants.TEXT_ELEMENT_NOT_FOUND);
+					}
 				}catch(Exception e){
 					log.debug("Exception in Text Migration");
 					sb.append(Constants.EXCEPTION_TEXT_COMPONENT);
@@ -145,51 +184,42 @@ public class PartnerVariation1 extends BaseAction {
 	}
 
 
-	private void migrateHero(Element heroEle, Node partnerLeftNode, String locale, Map<String, String> urlMap) throws PathNotFoundException, RepositoryException {
-		if(heroEle != null){
-			Node heroNode = partnerLeftNode.hasNode("hero_large")?partnerLeftNode.getNode("hero_large"):null;
-			if(heroNode != null){
-				Node heroPanelNode = heroNode.hasNode("heropanel_0")?heroNode.getNode("heropanel_0"):null;
-				if(heroPanelNode != null){
-					Element h2hero = heroEle.getElementsByTag("h2").first();
-					if(h2hero != null){
-						heroPanelNode.setProperty("title", h2hero.text());
-					}else{
-						sb.append(Constants.HERO_CONTENT_HEADING_ELEMENT_DOESNOT_EXISTS);
-					}
-					Element pHero = heroEle.getElementsByTag("p").first();
-					if(pHero != null){
-						heroPanelNode.setProperty("description", pHero.text());
-					}else{
-						sb.append(Constants.HERO_CONTENT_DESCRIPTION_ELEMENT_DOESNOT_EXISTS);
-					}
-					Element aHero = heroEle.getElementsByTag("p").last().getElementsByTag("a").first();
-					if(aHero != null){
-						heroPanelNode.setProperty("linktext", aHero.text());
-						heroPanelNode.setProperty("linkurl", FrameworkUtils.getLocaleReference(aHero.absUrl("href"), urlMap));
-					}else{
-						sb.append(Constants.HERO_CONTENT_ANCHOR_ELEMENT_DOESNOT_EXISTS);
-					}
-					String heroImage = FrameworkUtils.extractImagePath(heroEle, sb);
-					heroImage = FrameworkUtils.migrateDAMContent(heroImage, "", locale, sb);
-					if(heroImage != ""){
-						Node heroImageNode = heroPanelNode.hasNode("image")?heroPanelNode.getNode("image"):null;
-						if(heroImageNode != null){
-							heroImageNode.setProperty("fileReference", heroImage);
-						}else{
-							sb.append(Constants.HERO_IMAGE_NODE_NOT_FOUND);
-						}
-					}else{
-						sb.append(Constants.HERO_IMAGE_NOT_AVAILABLE);
-					}
+	private void migrateHero(Node heroNode , Element heroEle , String locale , Map<String, String> urlMap) throws PathNotFoundException, RepositoryException {
+		Node heroPanelNode = heroNode.hasNode("heropanel_0")?heroNode.getNode("heropanel_0"):null;
+		if(heroPanelNode != null){
+			Element h2hero = heroEle.getElementsByTag("h2").first();
+			if(h2hero != null){
+				heroPanelNode.setProperty("title", h2hero.text());
+			}else{
+				sb.append(Constants.HERO_CONTENT_HEADING_ELEMENT_DOESNOT_EXISTS);
+			}
+			Element pHero = heroEle.getElementsByTag("p").first();
+			if(pHero != null){
+				heroPanelNode.setProperty("description", pHero.text());
+			}else{
+				sb.append(Constants.HERO_CONTENT_DESCRIPTION_ELEMENT_DOESNOT_EXISTS);
+			}
+			Element aHero = heroEle.getElementsByTag("p").last().getElementsByTag("a").first();
+			if(aHero != null){
+				heroPanelNode.setProperty("linktext", aHero.text());
+				heroPanelNode.setProperty("linkurl", FrameworkUtils.getLocaleReference(aHero.absUrl("href"), urlMap));
+			}else{
+				sb.append(Constants.HERO_CONTENT_ANCHOR_ELEMENT_DOESNOT_EXISTS);
+			}
+			String heroImage = FrameworkUtils.extractImagePath(heroEle, sb);
+			heroImage = FrameworkUtils.migrateDAMContent(heroImage, "", locale, sb);
+			if(heroImage != ""){
+				Node heroImageNode = heroPanelNode.hasNode("image")?heroPanelNode.getNode("image"):null;
+				if(heroImageNode != null){
+					heroImageNode.setProperty("fileReference", heroImage);
 				}else{
-					sb.append(Constants.HERO_CONTENT_NODE_NOT_FOUND);
+					sb.append(Constants.HERO_IMAGE_NODE_NOT_FOUND);
 				}
 			}else{
-				sb.append(Constants.HERO_CONTENT_NODE_NOT_FOUND);
+				sb.append(Constants.HERO_IMAGE_NOT_AVAILABLE);
 			}
 		}else{
-			sb.append(Constants.HERO_CONTENT_PANEL_ELEMENT_NOT_FOUND);
+			sb.append(Constants.HERO_CONTENT_NODE_NOT_FOUND);
 		}
 
 	}
@@ -208,112 +238,125 @@ public class PartnerVariation1 extends BaseAction {
 	}
 
 	private void migrateHtmlBlobs(Element midEle, Node partnerMidNode,
-			String locale, Map<String, String> urlMap) throws RepositoryException {
-		if(midEle != null){
-			NodeIterator midNodes = partnerMidNode.hasNode("gd13v2-left")?partnerMidNode.getNodes("gd13v2-*"):null;
-			if(midNodes != null){
-				int size = (int)midNodes.getSize();
-				Elements htmlBlobEles = midEle.getElementsByClass("c00-pilot");
-				Elements htmlBlob_0_Eles = midEle.getElementsByClass("n13v12-pilot");
-				if(htmlBlobEles != null && htmlBlob_0_Eles != null){
-					int countSize = htmlBlob_0_Eles.size(),count=0;
-					int eleSize = htmlBlobEles.size();
-					if(size == eleSize){
-						for(Element htmlBlob : htmlBlobEles){
-							Node midNode = midNodes.nextNode();
-							if(midNode.hasNode("htmlblob")){
-								Node htmlBlobNode = midNode.getNode("htmlblob");
-								htmlBlobNode.setProperty("html", FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap));
-							}else{
-								sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
+			String locale, Map<String, String> urlMap, boolean loggedIn) throws RepositoryException {
+		NodeIterator midNodes = partnerMidNode.hasNode("gd13v2-left")?partnerMidNode.getNodes("gd13v2-*"):null;
+		if(midNodes != null){
+			int size = (int)midNodes.getSize();
+			Elements htmlBlobEles = midEle.getElementsByClass("c00-pilot");
+			Elements htmlBlob_0_Eles = midEle.getElementsByClass("n13v12-pilot");
+			if(htmlBlobEles != null && htmlBlob_0_Eles != null){
+				int countSize = htmlBlob_0_Eles.size(),count=0;
+				int eleSize = htmlBlobEles.size();
+				if(size == eleSize){
+					for(Element htmlBlob : htmlBlobEles){
+						Node midNode = midNodes.nextNode();
+						if(midNode.hasNode("htmlblob")){
+							Node htmlBlobNode = midNode.getNode("htmlblob");
+							String outerHtml = FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap);
+							if(!loggedIn){
+								outerHtml = outerHtml.replace("<p", "<!--<p").replace("p>", "p>-->");
 							}
-							if(midNode.hasNode("htmlblob_0")){
-								if(count < countSize){
-									htmlBlob = htmlBlob_0_Eles.get(count);
-									count++;
-								}else{
-									sb.append(Constants.HTMLBLOB_ELEMENT_NOT_FOUND);
-								}
-								Node htmlBlobNode = midNode.getNode("htmlblob_0");
-								htmlBlobNode.setProperty("html",  FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap));
-							}else{
-								sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
-							}
+							htmlBlobNode.setProperty("html", outerHtml);
+						}else{
+							sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
 						}
-					}else if(size > eleSize){
-						for(Element htmlBlob : htmlBlobEles){
-							Node midNode = midNodes.nextNode();
-							if(midNode.hasNode("htmlblob")){
-								Node htmlBlobNode = midNode.getNode("htmlblob");
-								htmlBlobNode.setProperty("html",  FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap));
+						if(midNode.hasNode("htmlblob_0")){
+							if(count < countSize){
+								htmlBlob = htmlBlob_0_Eles.get(count);
+								count++;
 							}else{
-								sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
+								sb.append(Constants.HTMLBLOB_ELEMENT_NOT_FOUND);
 							}
-							if(midNode.hasNode("htmlblob_0")){
-								if(count < countSize){
-									htmlBlob = htmlBlob_0_Eles.get(count);
-									count++;
-								}else{
-									sb.append(Constants.HTMLBLOB_ELEMENT_NOT_FOUND);
-								}
-								Node htmlBlobNode = midNode.getNode("htmlblob_0");
-								htmlBlobNode.setProperty("html",  FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap));
-							}else{
-								sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
+							Node htmlBlobNode = midNode.getNode("htmlblob_0");
+							if(!loggedIn){
+								htmlBlob.getElementsByTag("img").remove();
 							}
-						}
-						if(midNodes.hasNext()){
-							sb.append(Constants.MISMATCH_IN_HTMLBLOB_NODES+eleSize+Constants.HTMLBLOB_NODES_COUNT+size+".</li>");
-						}
-					}else if(size < eleSize){
-						for(Element htmlBlob : htmlBlobEles){
-							if(midNodes.hasNext()){
-								Node midNode = midNodes.nextNode();
-								if(midNode.hasNode("htmlblob")){
-									Node htmlBlobNode = midNode.getNode("htmlblob");
-									htmlBlobNode.setProperty("html",  FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap));
-								}else{
-									sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
-								}
-								if(midNode.hasNode("htmlblob_0")){
-									if(count < countSize){
-										htmlBlob = htmlBlob_0_Eles.get(count);
-										count++;
-									}else{
-										sb.append(Constants.HTMLBLOB_ELEMENT_NOT_FOUND);
-									}
-									Node htmlBlobNode = midNode.getNode("htmlblob_0");
-									htmlBlobNode.setProperty("html",  FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap));
-								}else{
-									sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
-								}
-							}else{
-								sb.append(Constants.MISMATCH_IN_HTMLBLOB_ELEMENT+eleSize+Constants.HTMLBLOB_NODES_COUNT+size+".</li>");
-							}
+							htmlBlobNode.setProperty("html",  FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap));
+						}else{
+							sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
 						}
 					}
-				}else{
-					sb.append(Constants.HTMLBLOB_CONTENT_DOES_NOT_EXIST);
+				}else if(size > eleSize){
+					for(Element htmlBlob : htmlBlobEles){
+						Node midNode = midNodes.nextNode();
+						if(midNode.hasNode("htmlblob")){
+							Node htmlBlobNode = midNode.getNode("htmlblob");
+							String outerHtml = FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap);
+							if(!loggedIn){
+								outerHtml = outerHtml.replace("<p", "<!--<p").replace("p>", "p>-->");
+							}
+							htmlBlobNode.setProperty("html", outerHtml);
+						}else{
+							sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
+						}
+						if(midNode.hasNode("htmlblob_0")){
+							if(count < countSize){
+								htmlBlob = htmlBlob_0_Eles.get(count);
+								count++;
+							}else{
+								sb.append(Constants.HTMLBLOB_ELEMENT_NOT_FOUND);
+							}
+							Node htmlBlobNode = midNode.getNode("htmlblob_0");
+							if(!loggedIn){
+								htmlBlob.getElementsByTag("img").remove();
+							}
+							htmlBlobNode.setProperty("html",  FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap));
+						}else{
+							sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
+						}
+					}
+					if(midNodes.hasNext()){
+						sb.append(Constants.MISMATCH_IN_HTMLBLOB_NODES+eleSize+Constants.HTMLBLOB_NODES_COUNT+size+".</li>");
+					}
+				}else if(size < eleSize){
+					for(Element htmlBlob : htmlBlobEles){
+						if(midNodes.hasNext()){
+							Node midNode = midNodes.nextNode();
+							if(midNode.hasNode("htmlblob")){
+								Node htmlBlobNode = midNode.getNode("htmlblob");
+								String outerHtml = FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap);
+								if(!loggedIn){
+									outerHtml = outerHtml.replace("<p", "<!--<p").replace("p>", "p>-->");
+								}
+								htmlBlobNode.setProperty("html", outerHtml);
+							}else{
+								sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
+							}
+							if(midNode.hasNode("htmlblob_0")){
+								if(count < countSize){
+									htmlBlob = htmlBlob_0_Eles.get(count);
+									count++;
+								}else{
+									sb.append(Constants.HTMLBLOB_ELEMENT_NOT_FOUND);
+								}
+								Node htmlBlobNode = midNode.getNode("htmlblob_0");
+								if(!loggedIn){
+									htmlBlob.getElementsByTag("img").remove();
+								}
+								htmlBlobNode.setProperty("html",  FrameworkUtils.extractHtmlBlobContent(htmlBlob, "", locale, sb, urlMap));
+							}else{
+								sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
+							}
+						}else{
+							sb.append(Constants.MISMATCH_IN_HTMLBLOB_ELEMENT+eleSize+Constants.HTMLBLOB_NODES_COUNT+size+".</li>");
+						}
+					}
 				}
 			}else{
-				sb.append(Constants.HTMLBLOB_NODE_DOES_NOT_EXIST);
+				sb.append(Constants.HTMLBLOB_CONTENT_DOES_NOT_EXIST);
 			}
 		}else{
-			sb.append(Constants.HTMLBLOB_CONTENT_DOES_NOT_EXIST);
+			sb.append(Constants.HTMLBLOB_NODE_DOES_NOT_EXIST);
 		}
 	}
 
 	private void migrateText(Element textEle, Node partnerBottomNode,
 			String locale, Map<String, String> urlMap) throws ValueFormatException, VersionException, LockException, ConstraintViolationException, RepositoryException {
-		if(textEle != null){
-			Node textNode = partnerBottomNode.hasNode("text_0")?partnerBottomNode.getNode("text_0"):null;
-			if(textNode != null){
-				textNode.setProperty("text", FrameworkUtils.extractHtmlBlobContent(textEle, "", locale, sb, urlMap));
-			}else{
-				sb.append(Constants.TEXT_NODE_NOT_FOUND);
-			}
+		Node textNode = partnerBottomNode.hasNode("text_0")?partnerBottomNode.getNode("text_0"):null;
+		if(textNode != null){
+			textNode.setProperty("text", FrameworkUtils.extractHtmlBlobContent(textEle, "", locale, sb, urlMap));
 		}else{
-			sb.append(Constants.TEXT_ELEMENT_NOT_FOUND);
+			sb.append(Constants.TEXT_NODE_NOT_FOUND);
 		}
 	}
 
