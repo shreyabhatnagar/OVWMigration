3cd0bc788e32895ed215d5bfabf3812ad30f637d
devalkar <rdevalka@cisco.com>
2016-01-26 19:38:39 +0530

    Fix for image migration for second run. - Kumari

--- OVWMigration/src/main/com/cisco/dse/global/migration/config/FrameworkUtils.java
+++ OVWMigration/src/main/com/cisco/dse/global/migration/config/FrameworkUtils.java
@@ -224,7 +224,7 @@ public class FrameworkUtils {
 					}
 					//if there is fileReference property in the wem, then updating the path with 'assets' will be replaced with 'global/locale' and 'en/us' will be replaced with 'global/locale'.
 					//if the fileReference property in the wem is blank then the web page image url, 'web' will be replaced with 'content/dam/global/locale', if there is no string 'web' image path then directly appending '/content/dam/global/locale' 
-					if (StringUtils.isNotBlank(imgRef)) {
+					if (StringUtils.isNotBlank(imgRef) && imgRef.indexOf(locale) == -1) {
 						imgRef = imgRef.replace("/assets", "/global/" + locale);
 						imgRef = imgRef.replace("/en/us", "/global/" + locale);
 					} else {
