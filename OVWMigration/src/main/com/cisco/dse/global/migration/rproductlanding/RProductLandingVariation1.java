b80662d77924e3d6f025bda78df6e8f53d4a9c6c
shreyabhatnagar <simshreya@gmail.com>
2016-01-27 12:30:41 +0530

    updated to fix issues

--- OVWMigration/src/main/com/cisco/dse/global/migration/rproductlanding/RProductLandingVariation1.java
+++ OVWMigration/src/main/com/cisco/dse/global/migration/rproductlanding/RProductLandingVariation1.java
@@ -516,7 +516,7 @@ public class RProductLandingVariation1 extends BaseAction {
 											sb.append("<li>video pop up is present in WEM page but it is not present in WEB page.</li>");
 										}
 										if (heroPanelPopUpNode != null && lightBoxElement != null && StringUtils.isNotBlank(heroPanelTitle)) {
-											heroPanelPopUpNode.setProperty("popupHeader", heroPanelTitle);
+											heroPanelPopUpNode.setProperty("header", heroPanelTitle);
 										}
 										// end hero pop up
 									}
