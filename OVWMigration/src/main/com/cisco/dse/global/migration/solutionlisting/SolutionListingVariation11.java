c948174a122cb91317970de6d60f29f142804840
azizuddin khan <azizukha@cisco.com>
2016-01-28 15:23:54 +0530

    Updated files after fixes for productlanding/ProductLandingVariation6.java and solutionlisting/SolutionListingVariation11.java

--- OVWMigration/src/main/com/cisco/dse/global/migration/solutionlisting/SolutionListingVariation11.java
+++ OVWMigration/src/main/com/cisco/dse/global/migration/solutionlisting/SolutionListingVariation11.java
@@ -87,7 +87,7 @@ public class SolutionListingVariation11 extends BaseAction {
 
 
 				// start set text component.
-				try {
+				/*try {
 					Elements textElements = doc.select("div.c00v0-alt1-pilot");
 					if (textElements != null) {
 						Node textNode = indexMidLeftNode.hasNode("text") ? indexMidLeftNode	.getNode("text"): null;
@@ -162,8 +162,24 @@ public class SolutionListingVariation11 extends BaseAction {
 				} catch (Exception e) {
 					sb.append("<li>" + Constants.EXCEPTION_TEXT_COMPONENT
 							+ e + "</li>");
-				}
+				}*/
 
+				try{
+					Element textEle = doc.select("div.c00v0-pilot").first();
+					if(textEle != null){
+						if(indexMidLeftNode.hasNode("text")){
+							Node textNode = indexMidLeftNode.getNode("text");
+							textNode.setProperty("text", textEle.html());
+						}else{
+							sb.append(Constants.TEXT_NODE_NOT_FOUND);
+						}
+					}else{
+						sb.append(Constants.TEXT_ELEMENT_NOT_FOUND);
+					}
+				}catch(Exception e){
+					sb.append(Constants.EXCEPTION_TEXT_COMPONENT);
+					log.error("Unable to migrate text component."+e);
+				}
 
 				// end set text
 				// ---------------------------------------------------------------------------------------------------------------------------------------
