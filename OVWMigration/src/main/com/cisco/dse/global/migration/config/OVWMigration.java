da3c769a3448bf4d4238a6b3a74830d6be216366
azizuddin khan <azizukha@cisco.com>
2016-01-29 16:26:23 +0530

    Added file for technology/TechnologyVariation1.java

--- OVWMigration/src/main/com/cisco/dse/global/migration/config/OVWMigration.java
+++ OVWMigration/src/main/com/cisco/dse/global/migration/config/OVWMigration.java
@@ -1151,8 +1151,19 @@ public class OVWMigration {
 																sb.append("<tr bgcolor='#888888'><th style='width:500px'>WEM url</th><th style='width:500px'>Web Publisher url</th><th style='width:500px'>Comments</th></tr>");
 																sb.append(msg16);
 																sb.append("<tr><td colspan='3'>.</td></tr>");
-															}
+															}else if ("technology-var1".equals(type)&&"YES".equalsIgnoreCase(check)) {
+																String msg16 = "";
+																msg16 = msg16 + "<tr>";
+																msg16 = msg16
+																		+ new TechnologyVariation1().translate(
+																				host, gLink, prod, type, cattype,
+																				sheet.getSheetName(), session, urlMap);
+																msg16 = msg16 + "</tr>";
 
+																sb.append("<tr bgcolor='#888888'><th style='width:500px'>WEM url</th><th style='width:500px'>Web Publisher url</th><th style='width:500px'>Comments</th></tr>");
+																sb.append(msg16);
+																sb.append("<tr><td colspan='3'>.</td></tr>");
+															}
 
 
 
