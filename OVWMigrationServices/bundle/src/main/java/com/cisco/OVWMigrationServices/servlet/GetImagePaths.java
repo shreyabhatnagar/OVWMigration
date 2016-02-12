2f0411d3ae63ad79a0c13130e8b101d5e8b7b947
suryakanth <sbasta@cisco.com>
2016-02-12 13:38:59 +0530

    Updated code to create the dam content package.

--- OVWMigrationServices/bundle/src/main/java/com/cisco/OVWMigrationServices/servlet/GetImagePaths.java
+++ OVWMigrationServices/bundle/src/main/java/com/cisco/OVWMigrationServices/servlet/GetImagePaths.java
@@ -1,43 +1,43 @@
 package com.cisco.OVWMigrationServices.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.sql.Timestamp;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import javax.jcr.Node;
 import javax.jcr.Session;
-import javax.jcr.Value;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
-import org.apache.sling.commons.json.JSONArray;
-import org.apache.sling.commons.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.api.DamConstants;
 import com.day.cq.dam.commons.util.AssetReferenceSearch;
-import com.day.cq.search.PredicateGroup;
-import com.day.cq.search.Query;
 import com.day.cq.search.QueryBuilder;
-import com.day.cq.search.result.SearchResult;
+import com.day.jcr.vault.fs.api.PathFilterSet;
+import com.day.jcr.vault.fs.api.ProgressTrackerListener;
+import com.day.jcr.vault.fs.config.DefaultWorkspaceFilter;
+import com.day.jcr.vault.packaging.JcrPackage;
+import com.day.jcr.vault.packaging.JcrPackageDefinition;
+import com.day.jcr.vault.packaging.JcrPackageManager;
+import com.day.jcr.vault.packaging.PackagingService;
+import com.day.jcr.vault.util.DefaultProgressListener;
 
 @SlingServlet(paths = { "/bin/services/GetimagePaths" }, methods = { "GET" })
 @Properties({ @Property(name = "service.pid", value = "com.cisco.OVWMigrationServices.servlet.GetImagePaths") })
@@ -57,154 +57,87 @@ public class GetImagePaths extends SlingAllMethodsServlet {
 	@Reference
 	private QueryBuilder builder;
 
-	protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
+	protected void doPost(SlingHttpServletRequest request,
+			SlingHttpServletResponse response) throws IOException {
+
+		String packageName = "my package";
+		String packageGroupName = "OVW-Migration";
 
 		PrintWriter printWritter = response.getWriter();
 		ResourceResolver resourceResolver = null;
-		JSONArray ja = new JSONArray();
 		Session session = null;
-		String pagePath = request.getParameter("pagePath");
-		Map<String, String> queryMap = new HashMap<String, String>();
-		Set<String> set = new HashSet<String>();
-		
+		XSSFWorkbook workbook = null;
 		try {
-			resourceResolver = resolverFactory.getAdministrativeResourceResolver(null);
-			Resource r = resourceResolver.getResource(pagePath+"/jcr:content");
-			Node n = r.adaptTo(Node.class);
-			AssetReferenceSearch ref = new AssetReferenceSearch(n, DamConstants.MOUNTPOINT_ASSETS, resourceResolver);
-			Map<String, Asset> allref = new HashMap<String, Asset>();
-			allref.putAll(ref.search());
-			for (Map.Entry<String, Asset> entry : allref.entrySet()) {
-				String val = entry.getKey();
-				Asset asset = entry.getValue();
-				printWritter.print("\nassets : "+asset.getPath());
-			}
-		}catch(Exception e){
-			printWritter.print("Exception : " + e);
-		}
-		
-		/*
-		try {
-			resourceResolver = resolverFactory.getAdministrativeResourceResolver(null);
+			resourceResolver = resolverFactory
+					.getAdministrativeResourceResolver(null);
 			session = resourceResolver.adaptTo(Session.class);
-			queryMap.put("path", pagePath);
-			queryMap.put("type", "nt:unstructured");
-			queryMap.put("property", "fileReference");
-			queryMap.put("property.operation", "exists");
-			queryMap.put("property.value", "true");
-			Query query = builder.createQuery(PredicateGroup.create(queryMap), session);
-			SearchResult result = query.getResult();
-			Iterator<Node> groupNodes = result.getNodes();
-			Node groupNode = null;
-			while (groupNodes.hasNext()) {
-				groupNode = groupNodes.next();
-				String fileReference = groupNode.hasProperty("fileReference") ? groupNode.getProperty("fileReference").getString() : "pathNotFound";
-				set.add(fileReference);
+			JcrPackageManager packageManager = (JcrPackageManager) PackagingService
+					.getPackageManager(session);
+			/*
+			 * For 'create' method the parameter packageGroup is optional we can
+			 * give group name under which the package should be created else it
+			 * will take default, packageName is the name of the package and 1.0
+			 * is the version of the package
+			 */
+			java.util.Date date = new java.util.Date();
+			JcrPackage pack = packageManager
+					.create(packageGroupName,
+							packageName
+									+ "-"
+									+ new Timestamp(date.getTime()).toString()
+											.replace(":", "-")
+											.replace(".", "-"), "1.0");
+			JcrPackageDefinition definition = pack.getDefinition();
+
+			DefaultWorkspaceFilter filter = new DefaultWorkspaceFilter();
+			/* nodePaths is the List containing the list of paths */
+			
+			
+			Node ntFileNode = session.getNode("/content/dam/assets/OVWDEMO_DAM_ASSETS.xlsx/jcr:content/renditions/original/jcr:content"); 
+			InputStream is = ntFileNode.getProperty("jcr:data").getBinary().getStream();
+			workbook = new XSSFWorkbook(is);
+			int count = 0;
+			int totAssets = 0;
+			for (XSSFSheet sheet : workbook) {
+				for (Row tempRow : sheet) {
+					if(count == 0){
+					String pagePath = tempRow.getCell(0) != null ? tempRow
+							.getCell(0).getStringCellValue() : "";
+					printWritter.print("\n------------------------------------------");		
+					printWritter.print("\npage path : " + pagePath);
+					Resource r = resourceResolver.getResource(pagePath
+							+ "/jcr:content");
+					Node n = r.adaptTo(Node.class);
+					AssetReferenceSearch ref = new AssetReferenceSearch(n,
+							DamConstants.MOUNTPOINT_ASSETS, resourceResolver);
+					Map<String, Asset> allref = new HashMap<String, Asset>();
+					allref.putAll(ref.search());
+					for (Map.Entry<String, Asset> entry : allref.entrySet()) {
+						Asset asset = entry.getValue();
+						printWritter.print("\nassets : " + asset.getPath());
+						PathFilterSet pathFilterSet = new PathFilterSet();
+						pathFilterSet.setRoot(asset.getPath());
+						filter.add(pathFilterSet);
+						totAssets++;
+					}
+					}
+					if(count == 1){
+						String pagePath = tempRow.getCell(0) != null ? tempRow
+								.getCell(0).getStringCellValue() : "";
+						PathFilterSet pathFilterSet = new PathFilterSet();
+						pathFilterSet.setRoot(pagePath);
+						filter.add(pathFilterSet);		
+					}
+				}
+				count++;
 			}
+			definition.setFilter(filter, true);
+			ProgressTrackerListener listener = new DefaultProgressListener();
+			packageManager.assemble(pack, listener);
+			printWritter.print("\nTotal assets : "+totAssets);
 		} catch (Exception e) {
 			printWritter.print("Exception : " + e);
 		}
-		try {
-			queryMap = new HashMap<String, String>();
-			queryMap.put("path", pagePath);
-			queryMap.put("type", "nt:unstructured");
-			queryMap.put("property", "listitems");
-			queryMap.put("property.operation", "exists");
-			queryMap.put("property.value", "true");
-			Query query = builder.createQuery(PredicateGroup.create(queryMap), session);
-			SearchResult result = query.getResult();
-			Iterator<Node> groupNodes = result.getNodes();
-			Node groupNode = null;
-			while (groupNodes.hasNext()) {
-				groupNode = groupNodes.next();
-				javax.jcr.Property listitems = groupNode.hasProperty("listitems") ? groupNode.getProperty("listitems") : null;
-				if(listitems.isMultiple()){
-					Value[] values = listitems.getValues();
-					for(Value val : values){
-						String data = val.getString();
-						if(StringUtils.isNotBlank(data)){
-							JSONObject obj = new JSONObject(data);
-							String url = (String)(obj.has("linkurl")?obj.get("linkurl"):"");
-							if(url.endsWith(".pdf") || url.endsWith(".doc") || url.endsWith(".docx") || url.endsWith(".PDF") || url.endsWith(".DOC") || url.endsWith(".DOCX")){
-								if(resourceResolver.getResource(url) != null){
-									set.add(url);
-								}
-							}
-						}
-					}
-				}else{
-					String data = listitems.getValue().getString();
-					if(StringUtils.isNotBlank(data)){
-						JSONObject obj = new JSONObject(data);
-						String url = (String)(obj.has("linkurl")?obj.get("linkurl"):"");
-						if(url.endsWith(".pdf") || url.endsWith(".doc") || url.endsWith(".docx") || url.endsWith(".PDF") || url.endsWith(".DOC") || url.endsWith(".DOCX")){
-							if(resourceResolver.getResource(url) != null){
-								set.add(url);
-							}
-						}
-					}
-				}
-			}
-		} catch (Exception e) {
-			printWritter.print("Exception : " + e);
-		}
-		
-		
-		set.addAll(getImagePathsFromHtml("html", pagePath, session, resourceResolver));// get the html content from the html property to get the image paths.
-
-		set.addAll(getImagePathsFromHtml("text", pagePath, session, resourceResolver));// get the html content from the text property to get the image paths.
-
-		try {
-			for (String val : set) {
-				ja.put(val);
-			}
-		} catch (Exception e) {
-			printWritter.print("Exception : " + e);
-		}
-		printWritter.print(ja.toString());
-		*/
-	}
-
-	Set<String> getImagePathsFromHtml(String propName, String pagePath,
-			Session session, ResourceResolver resourceResolver) {
-
-		Map<String, String> queryMap = new HashMap<String, String>();
-		Set<String> set = new HashSet<String>();
-		try {
-			queryMap.put("path", pagePath);
-			queryMap.put("type", "nt:unstructured");
-			queryMap.put("property", propName);
-			queryMap.put("property.operation", "exists");
-			queryMap.put("property.value", "true");
-
-			Query query = builder.createQuery(PredicateGroup.create(queryMap), session);
-			SearchResult result = query.getResult();
-			Iterator<Node> groupNodes = result.getNodes();
-			Node groupNode = null;
-			while (groupNodes.hasNext()) {
-				groupNode = groupNodes.next();
-				String html = groupNode.hasProperty(propName) ? groupNode
-						.getProperty(propName).getString() : "pathNotFound";
-
-				Document document = Jsoup.parse(html);
-				for (Element element : document.getElementsByTag("img")) {
-					String src = element.attr("src");
-					set.add(src);
-				}
-				
-				for (Element element : document.getElementsByTag("a")) {
-					String url = element.attr("href");
-					if(url.endsWith(".pdf") || url.endsWith(".doc") || url.endsWith(".docx") || url.endsWith(".PDF") || url.endsWith(".DOC") || url.endsWith(".DOCX")){
-						if(resourceResolver.getResource(url) != null){
-							set.add(url);
-						}
-					}
-				}
-			}
-		} catch (Exception e) {
-			set.add(e.getMessage());
-		}
-		return set;
 	}
 
 	protected void doGet(SlingHttpServletRequest request,
