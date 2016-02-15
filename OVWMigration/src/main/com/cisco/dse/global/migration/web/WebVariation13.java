0dab9e45f1f445dcac99ee8222005aaeeed6eaf2
sreevidyas <asdf1234$>
2016-02-15 17:27:03 +0530

    updated the web var 13 java file

--- OVWMigration/src/main/com/cisco/dse/global/migration/web/WebVariation13.java
+++ OVWMigration/src/main/com/cisco/dse/global/migration/web/WebVariation13.java
@@ -28,6 +28,7 @@ import com.cisco.dse.global.migration.config.FrameworkUtils;
 
 public class WebVariation13 extends BaseAction {
 	Document doc;
+	Document securedDoc;
 	StringBuilder sb = new StringBuilder(1024);
 	static Logger log = Logger.getLogger(WebVariation13.class);
 	
@@ -48,22 +49,33 @@ public class WebVariation13 extends BaseAction {
 		pagePropertiesPath = pagePropertiesPath.replace("<locale>", locale);
 		String webNodeTopPath = pagePropertiesPath+"/content_parsys/solutions/layout-solutions/gd11v1";
 		String webNodeBottomPath = pagePropertiesPath+"/content_parsys/solutions/layout-solutions/gd12v2";
-
+		String webNodeSecuredTopPath = pagePropertiesPath+"/content_parsys/solutions/layout-solutions/gd11v1_0";
+		String webNodeSecuredBottomPath = pagePropertiesPath+"/content_parsys/solutions/layout-solutions/gd12v2_0";
 		sb.append("<td>" + "<a href="+pageUrl+">"+pageUrl+"</a>"+"</td>");
 		sb.append("<td>" + "<a href="+loc+">"+loc +"</a>"+ "</td>");
 		sb.append("<td><ul>");
 		
 		javax.jcr.Node webTopNode = null;
 		javax.jcr.Node webBottomNode = null;
+		javax.jcr.Node webSecuredTopNode = null;
+		javax.jcr.Node webSecuredBottomNode = null;
 		javax.jcr.Node pageJcrNode = null;
 		try {
 			webTopNode = session.getNode(webNodeTopPath);
 			webBottomNode = session.getNode(webNodeBottomPath);
+			webSecuredTopNode = session.getNode(webNodeSecuredTopPath);
+			webSecuredBottomNode = session.getNode(webNodeSecuredBottomPath);
 			pageJcrNode = session.getNode(pagePropertiesPath);
 			try {
 				doc = getConnection(loc);
+				log.debug("document:"+doc.html());
 			} catch (Exception e) {
 				log.error("Exception ", e);
+			}
+			try{
+				securedDoc = getSecuredConnection(loc);
+			}catch(Exception e){
+				log.error("Exception : ",e);
 			}
 			if (doc != null) {
 				// ------------------------------------------------------------------------------------------------------------------------------------------
@@ -105,6 +117,33 @@ public class WebVariation13 extends BaseAction {
 					} else {
 						sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
 					}
+				
+					log.debug("htmlBlobContent migrated is done." + htmlBlobContent);
+					// secure 
+					if (securedDoc != null) {
+						String securedHtmlBlobContent = "";
+						Elements securedhtmlBlobTitle = securedDoc.select("div#mb-title-nav-bar");
+						if (securedhtmlBlobTitle != null && !htmlBlobTitle.isEmpty()) {
+							securedHtmlBlobContent = FrameworkUtils.extractHtmlBlobContent(securedhtmlBlobTitle.last(), "", locale, sb, urlMap);
+							oldImage.append(securedHtmlBlobContent);
+						}
+						Elements securedhtmlBlobElements = doc.select("div#location_wrapper");
+						if (securedhtmlBlobElements != null && !htmlBlobElements.isEmpty()) {
+							securedHtmlBlobContent = FrameworkUtils.extractHtmlBlobContent(securedhtmlBlobElements.first(), "", locale, sb, urlMap);
+								oldImage.append(securedHtmlBlobContent);
+							}
+						log.debug("securedHtmlBlobContent migrated is done." + securedHtmlBlobContent);
+						if (webSecuredTopNode.hasNode("gd11v1-mid/htmlblob")) {
+							Node htmlBlobNode = webSecuredTopNode.getNode("gd11v1-mid/htmlblob");
+							if (StringUtils.isNotBlank(securedHtmlBlobContent)) {
+								htmlBlobNode.setProperty("html",oldImage.toString());
+								log.debug("securedHtmlBlob Content migration is done.");
+							}
+						} else {
+							sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
+						}
+					}
+					// secure
 				}	
 				catch (Exception e) {
 					sb.append(Constants.EXCEPTION_IN_HTMLBLOB);
@@ -135,6 +174,27 @@ public class WebVariation13 extends BaseAction {
 					} else {
 						sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
 					}
+					log.debug("htmlBlobContent second guest migrated is done." + htmlBlobContent);
+					// secure
+					if (securedDoc != null) { 
+						String securedHtmlBlobContent = "";
+						Elements securedHtmlBlobElements = securedDoc.select("div.guest");
+						if(securedHtmlBlobElements != null && !securedHtmlBlobElements.isEmpty()){
+							securedHtmlBlobContent = FrameworkUtils.extractHtmlBlobContent(securedHtmlBlobElements.first(), "", locale, sb, urlMap);
+						}
+						log.debug("securedHtmlBlobContent second guest migrated is done." + securedHtmlBlobContent);
+						
+						if (webSecuredBottomNode.hasNode("gd12v2-left/htmlblob")) {
+							Node htmlBlobNode = webSecuredBottomNode.getNode("gd12v2-left/htmlblob");
+							if (StringUtils.isNotBlank(securedHtmlBlobContent)) {
+								htmlBlobNode.setProperty("html",securedHtmlBlobContent);
+								log.debug("securedHtmlBlob Content migration is done.");
+							}
+						} else {
+							sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
+						}
+					}
+					// secure
 				}	
 				catch (Exception e) {
 					sb.append(Constants.EXCEPTION_IN_HTMLBLOB);
@@ -153,8 +213,8 @@ public class WebVariation13 extends BaseAction {
 					Elements htmlBlob4Ele  = htmlBlobElements.select("div#col3");
 					if (htmlBlob4Ele != null) {
 							htmlBlobContent = FrameworkUtils.extractHtmlBlobContent(htmlBlob4Ele.first(), "", locale, sb, urlMap);
-					}
-					
+						}
+					log.debug("htmlBlobContent third col3 migrated is done." + htmlBlobContent);
 					
 					//End of getContent
 					//Start of set content
@@ -167,6 +227,27 @@ public class WebVariation13 extends BaseAction {
 					} else {
 						sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
 					}
+					// secure
+					if (securedDoc != null) {
+						String securedHtmlBlobThirdContent = "";
+						Elements securedHtmlBlobElements = securedDoc.select("div.guest");
+						Elements securedHtmlBlob4Ele  = securedHtmlBlobElements.select("div#col3");
+						if (securedHtmlBlob4Ele != null) {
+							securedHtmlBlobThirdContent = FrameworkUtils.extractHtmlBlobContent(securedHtmlBlob4Ele.first(), "", locale, sb, urlMap);
+						}
+						log.debug("securedHtmlBlobContent third col3 migrated is done." + securedHtmlBlobThirdContent);
+						
+						if (webSecuredBottomNode.hasNode("gd12v2-right/htmlblob")) {
+							Node htmlBlobNode = webSecuredBottomNode.getNode("gd12v2-right/htmlblob");
+							if (StringUtils.isNotBlank(securedHtmlBlobThirdContent)) {
+								htmlBlobNode.setProperty("html",securedHtmlBlobThirdContent);
+								log.debug("securedHtmlBlob Content migration is done.");
+							}
+						} else {
+							sb.append(Constants.HTMLBLOB_NODE_NOT_FOUND);
+						}
+					}
+					// secure
 				}catch (Exception e) {
 					sb.append(Constants.EXCEPTION_IN_HTMLBLOB);
 					log.error("Exception " , e);
